55d6c5bfa8e013b3c81daa0502c13f489e566234
DwaynesWorld <kthompson713@gmail.com>
2018-08-05 20:55:29 -0500

    Project setup, added packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import * as React from 'react';
-import './App.css';
+import * as React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { Provider } from "react-redux";
 
-import logo from './logo.svg';
+import "./App.css";
+
+import { Home } from "./components/home";
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/" component={Home} />
+        </Switch>
+      </Router>
     );
   }
 }
